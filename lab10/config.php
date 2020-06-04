04842fc35402a2b0c6e21c692877c92fe17b6671
dapangqing <1059786998@qq.com>
2020-06-04 15:15:29 +0800

    a

---
+++ lab10/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBNAME', 'art');
+define('DBUSER', 'dpq');
+define('DBPASS', 'dapangqing');
+define('DBCONNSTRING','mysql:dbname=art;charset=utf8mb4;');
+?>
